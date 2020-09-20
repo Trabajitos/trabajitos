7f3ee669050faf79ddcde8a5a243d397d55d902a
n3gr0lc <lsb.marambio@gmail.com>
2020-09-20 10:19:11 -0300

    Autenticacion con firebase

---
+++ app/src/main/java/cl/trabajitos/intentodelogin/AuthActivity.kt
@@ -0,0 +1,45 @@
+package cl.trabajitos.intentodelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+        setup()
+    }
+
+    private fun setup() {
+        title = "Autentificación"
+        logInButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    emailEditText.text.toString(),
+                    passwordEditText.text.toString()).addOnCompleteListener{
+                    if(it.isSuccessful){
+                showHome(it.result?.user?.email?:" ", ProviderType.BASIC)
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    private fun showHome (email: String, provider: ProviderType){
+
+        val homeIntent :Intent = Intent(this,HomeActivity::class.java).apply{
+            putExtra("email",email)
+            putExtra("provider",provider.name)
+        }
+        startActivity(homeIntent)
+
+    }
+
+
+}
