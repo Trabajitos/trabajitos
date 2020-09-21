ecfe3ec7277fe3231c3c34baef6ae5f945e18ab3
n3gr0lc <lsb.marambio@gmail.com>
2020-09-21 17:19:28 -0300

    cambios en el registro

--- app/src/main/java/cl/trabajitos/intentodelogin/SignUpActivity.kt
+++ app/src/main/java/cl/trabajitos/intentodelogin/SignUpActivity.kt
@@ -2,6 +2,8 @@ package cl.trabajitos.intentodelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -13,8 +15,26 @@ class SignUpActivity : AppCompatActivity() {
 
     private fun setup() {
 
-        backButton.setOnClickListener{
+        backButton.setOnClickListener {
             onBackPressed()
         }
+
+        nextButton.setOnClickListener {
+
+
+            if (nameEditText.text.isNotEmpty() &&
+                lastNameEditText.text.isNotEmpty() &&
+                birthEditTextDate.text.isNotEmpty()
+            ) {
+
+                personalInfoLayout.visibility = View.GONE
+                contactInfoLayout.visibility = View.VISIBLE
+                pageTextView.setText("2 de 3")
+
+            }
+
+        }
     }
+
+
 }
