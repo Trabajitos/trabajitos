10f3877ddbd2382747d5f15be902f737e8785dae
n3gr0lc <lsb.marambio@gmail.com>
2020-09-22 21:14:16 -0300

    al tuto

--- app/src/main/java/cl/trabajitos/intentodelogin/SignUpActivity.kt
+++ app/src/main/java/cl/trabajitos/intentodelogin/SignUpActivity.kt
@@ -1,9 +1,11 @@
 package cl.trabajitos.intentodelogin
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.graphics.toColorInt
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_signup.*
 
@@ -24,29 +26,30 @@ class SignUpActivity : AppCompatActivity() {
 
 
             if (personalInfoLayout.visibility == View.VISIBLE &&
-                (nameEditText.text.isNotEmpty() &&
-                        lastNameEditText.text.isNotEmpty() &&
-                        birthEditTextDate.text.isNotEmpty()
-                        )
+                contactInfoLayout.visibility == View.GONE
             ) {
                 personalInfoLayout.visibility = View.GONE
                 contactInfoLayout.visibility = View.VISIBLE
-                pageTextView.setText("2 de 3")
+                pageTextView.setText("2 de 2")
+                typeTextView.setText("De Contacto")
 
             } else if (contactInfoLayout.visibility == View.VISIBLE &&
-                (emailEditText.text.isNotEmpty() &&
-                        phoneEditText.text.isNotEmpty() &&
-                        passwordEditText.text.isNotEmpty() &&
-                        confirmPasswordEditText.text.isNotEmpty()
-                        )
+                personalInfoLayout.visibility == View.GONE
             ) {
 
                 val intentCreateUser: Intent = Intent(this, CreateUserActivity::class.java)
                 startActivity(intentCreateUser)
-                
-
             }
 
+        }
+        backSignUpButton.setOnClickListener{
+            if(personalInfoLayout.visibility == View.GONE &&
+                    contactInfoLayout.visibility == View.VISIBLE){
+                personalInfoLayout.visibility = View.VISIBLE
+                contactInfoLayout.visibility = View.GONE
+                pageTextView.setText("1 de 2")
+                typeTextView.setText("Personales")
+            }
         }
     }
 
