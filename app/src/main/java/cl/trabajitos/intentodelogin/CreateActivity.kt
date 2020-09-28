f4fd8a42b78e66dcfbba6f5cbc8a103d55aa5d69
n3gr0lc <lsb.marambio@gmail.com>
2020-09-28 17:36:51 -0300

    commit

--- app/src/main/java/cl/trabajitos/intentodelogin/CreateActivity.kt
+++ app/src/main/java/cl/trabajitos/intentodelogin/CreateActivity.kt
@@ -2,10 +2,24 @@ package cl.trabajitos.intentodelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_create.*
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+        setUp()
+    }
+
+    private fun setUp(){
+        nextWorkButton.setOnClickListener {
+            firstLayout.visibility = View.GONE
+            secondLayout.visibility = View.VISIBLE
+        }
+        backWorkButton.setOnClickListener {
+            firstLayout.visibility = View.VISIBLE
+            secondLayout.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/cl/trabajitos/intentodelogin/SignUpActivity.kt
+++ app/src/main/java/cl/trabajitos/intentodelogin/SignUpActivity.kt
@@ -1,12 +1,9 @@
 package cl.trabajitos.intentodelogin
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.graphics.toColorInt
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -18,40 +15,46 @@ class SignUpActivity : AppCompatActivity() {
 
     private fun setup() {
 
+
         backButton.setOnClickListener {
             onBackPressed()
         }
-
         nextButton.setOnClickListener {
-
-
             if (personalInfoLayout.visibility == View.VISIBLE &&
                 contactInfoLayout.visibility == View.GONE
             ) {
-                personalInfoLayout.visibility = View.GONE
-                contactInfoLayout.visibility = View.VISIBLE
-                pageTextView.setText("2 de 2")
-                typeTextView.setText("De Contacto")
-
+                showPanelTwo()
+                backSignUpButton.visibility = View.VISIBLE
             } else if (contactInfoLayout.visibility == View.VISIBLE &&
                 personalInfoLayout.visibility == View.GONE
             ) {
-
-                val intentCreateUser: Intent = Intent(this, CreateUserActivity::class.java)
+                val intentCreateUser: Intent = Intent(this, CreateActivity::class.java)
                 startActivity(intentCreateUser)
             }
-
         }
-        backSignUpButton.setOnClickListener{
-            if(personalInfoLayout.visibility == View.GONE &&
-                    contactInfoLayout.visibility == View.VISIBLE){
-                personalInfoLayout.visibility = View.VISIBLE
-                contactInfoLayout.visibility = View.GONE
-                pageTextView.setText("1 de 2")
-                typeTextView.setText("Personales")
+
+        backSignUpButton.setOnClickListener {
+            if (personalInfoLayout.visibility == View.GONE &&
+                contactInfoLayout.visibility == View.VISIBLE
+            ) {
+                showPanelOne()
+                backSignUpButton.visibility = View.INVISIBLE
             }
         }
     }
 
+    private fun showPanelOne() {
+        contactInfoLayout.visibility = View.GONE
+        personalInfoLayout.visibility = View.VISIBLE
+        pageTextView.setText("1 de 2")
+        typeTextView.setText("Personales")
+    }
+
+    private fun showPanelTwo() {
+        personalInfoLayout.visibility = View.GONE
+        contactInfoLayout.visibility = View.VISIBLE
+        pageTextView.setText("2 de 2")
+        typeTextView.setText("De Contacto")
+    }
 
 }
