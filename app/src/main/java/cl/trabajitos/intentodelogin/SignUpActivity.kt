cbc2c97a90c02e87a5777486be83b0d2ad655509
n3gr0lc <lsb.marambio@gmail.com>
2020-10-13 19:33:05 -0300

    commit para la clase

--- app/src/main/java/cl/trabajitos/intentodelogin/SignUpActivity.kt
+++ app/src/main/java/cl/trabajitos/intentodelogin/SignUpActivity.kt
@@ -4,17 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignUpActivity : AppCompatActivity() {
+
+
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         setup()
+
     }
 
     private fun setup() {
-
 
         backButton.setOnClickListener {
             onBackPressed()
@@ -28,6 +37,14 @@ class SignUpActivity : AppCompatActivity() {
             } else if (contactInfoLayout.visibility == View.VISIBLE &&
                 personalInfoLayout.visibility == View.GONE
             ) {
+                db.collection("users").document(emailEditText.text.toString()).set(
+                    hashMapOf(
+                        "name" to nameEditText.text.toString(),
+                        "lastName" to lastNameEditText.text.toString(),
+                        "phone" to phoneEditText.text.toString(),
+                        "password" to passwordEditText.text.toString()
+                    )
+                )
                 val intentCreateUser: Intent = Intent(this, CreateActivity::class.java)
                 startActivity(intentCreateUser)
             }
@@ -41,20 +58,42 @@ class SignUpActivity : AppCompatActivity() {
                 backSignUpButton.visibility = View.INVISIBLE
             }
         }
+        val countries = resources.getStringArray(R.array.Countries)
+
+        val spinner = findViewById<Spinner>(R.id.countrySpinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, countries
+            )
+            spinner.adapter = adapter
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                }
+            }
+        }
     }
 
     private fun showPanelOne() {
         contactInfoLayout.visibility = View.GONE
         personalInfoLayout.visibility = View.VISIBLE
-        pageTextView.setText("1 de 2")
         typeTextView.setText("Personales")
+        pageTextView.setText("1 de 2")
     }
 
     private fun showPanelTwo() {
         personalInfoLayout.visibility = View.GONE
         contactInfoLayout.visibility = View.VISIBLE
-        pageTextView.setText("2 de 2")
         typeTextView.setText("De Contacto")
+        pageTextView.setText("2 de 2")
+
     }
 
 }
